60a100414d65568817c586810b6dfb529890d30c
rupeshace <rupeshace@gmail.com>
2019-11-04 12:13:50 +0800

    Create CustomerRepoService.java

---
+++ level3/Customer-journey/src/main/java/com/java/dao/CustomerRepoService.java
@@ -0,0 +1,12 @@
+package com.java.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CustomerRepoService {
+
+	@Autowired
+	private CustomerRepository mCustomerRepository;
+
+	// TODO implement all DAO api here.
+
+}
