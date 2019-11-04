935f6c518e747e694388f8b63524ba6677839fa6
rupeshace <rupeshace@gmail.com>
2019-11-04 11:03:01 +0800

    Create Recursion.java

---
+++ level1/Recursion.java
@@ -0,0 +1,12 @@
+class Recursion
+{
+    public static void Main(string[] args)
+    {
+        printNum(10);
+    }
+    static void printNum(int num)
+    {
+       //complete this method
+    }
+   
+}
